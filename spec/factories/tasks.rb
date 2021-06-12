52ab1112835f52790eb34ccdbe740b73e2eac3fc
Corine <corine@localhost.localdomain>
2021-06-12 13:55:33 +0100

    Write system spec

---
+++ spec/factories/tasks.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :task do
+    # Please change the following contents according to the column name actually created
+    task_name { 'test_taskname' }
+    description { 'test_description' }
+  end
+end
---
+++ spec/system/task_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+RSpec.describe 'Task management function', type: :system do
+  describe 'New creation function' do
+    context 'When creating a new task' do
+      it 'The created task is displayed' do
+        visit new_task_path
+        task = FactoryBot.create(:task, task_name: 'task')
+        expect(page).to have_content 'task'
+      end
+    end
+  end
+  describe 'List display function' do
+    context 'When transitioning to the list screen' do
+      it 'The created task list is displayed' do
+        task = FactoryBot.create(:task, task_name: 'task')
+        visit tasks_path
+        expect(page).to have_content 'task'
+      end
+    end
+  end
+  describe 'Detailed display function' do
+     context 'When transitioned to any task details screen' do
+       it 'The content of the relevant task is displayed' do
+        task = FactoryBot.create(:task, task_name: 'task')
+        visit task_path(task.id)
+        expect(page).to have_content 'task'
+       end
+     end
+  end
+end
