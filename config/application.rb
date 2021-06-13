822997967347d4f2bd90dcb2c5b9b7ab73bac421
Corine <corine@localhost.localdomain>
2021-06-13 12:03:04 +0100

    Setting time zone

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,9 @@ Bundler.require(*Rails.groups)
 module Everleaf
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.time_zone = 'London'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :fr
     config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
