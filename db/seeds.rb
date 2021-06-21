a33a18672bbea16b338ca6d199707a2a0cfd02ee
Corine <corine@localhost.localdomain>
2021-06-21 13:30:48 +0100

    Seed data 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,40 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
+require 'date'
+def time_rand from = 0.0, to = Time.now
+    Time.at(from + rand * (to.to_f - from.to_f))
+end
 
-User.create!(
-    id: 1, 
-    name: Faker::Name.name,
-    email: Faker::Internet.email,
-    password: "password", 
-    password_confirmation: "password",
-)
+@user_indice = User.last.id
+(@user_indice+1..@user_indice+10).each do |id|
+	User.create!(
+	    id: id, 
+	    name: Faker::Name.name,
+	    email: Faker::Internet.email,
+	    password: "password", 
+	    password_confirmation: "password",
+	)
+end
+
+@task_indice = Task.last.id
+(@task_indice+1..@task_indice+10).each do |id|
+	Task.create!(
+	    id: id, 
+	    task_name: Faker::Lorem.words,
+	    description: Faker::Lorem.sentence,
+	    status: ["completed","in progress","unstarted"].sample, 
+	    deadline: time_rand.year.to_s+"/"+time_rand.month.to_s+"/"+time_rand.day.to_s,
+	    priority: ["high", "medium","low"].sample,
+	    user_id: User.all.pluck(:id).sample,
+	)
+end
+
+@label_indice = Label.last.id
+(@label_indice+1..@label_indice+3).each do |id|
+    Label.create!(
+        id: id, 
+        title: Faker::Verb.past_participle,
+    )
+end
+
